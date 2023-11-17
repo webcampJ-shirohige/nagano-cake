7bf88e95ab5d13b2b911e320feef364f3fb3152f
zzriku <zzriku@users.noreply.github.com>
2023-11-17 09:05:43 +0000

    viewファイルの作成

--- app/controllers/public/adresses_controller.rb
+++ app/controllers/public/adresses_controller.rb
@@ -1,10 +1,18 @@
 class Public::AdressesController < ApplicationController
   def index
-    @address = Address.new
+    @address = Address.all
   end
 
   def create
-    @address = Address.new
+    @address = Address.new(address_params)
+    @address.customer_id = current_customer.id
+    if @address.save
+      redirect address_path, notice: "配送先の登録に成功しました。"
+    else
+      @address = current_customer.address
+      render :index
+      flash.now[:alert] = "配送先の登録に失敗しました。"
+    end
   end
 
   def edit
@@ -15,6 +23,12 @@ class Public::AdressesController < ApplicationController
   end
 
   def update
+  end
+
+  private
+
+  def address_params
+    params.require(:address).permit(:shipping_post_code, :shipping_address, :shipping_name)
   end
 
 end
