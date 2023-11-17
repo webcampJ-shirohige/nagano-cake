a5ea23b44ec56eae46183cef57f38f91291c7ff2
tomoya2051 <f.tomoya_2051@icloud.com>
2023-11-17 02:22:20 +0000

    Adminカラムの追加

--- db/migrate/20231117020907_create_admins.rb
+++ db/migrate/20231117020907_create_admins.rb
@@ -1,7 +1,8 @@
 class CreateAdmins < ActiveRecord::Migration[6.1]
   def change
     create_table :admins do |t|
-
+      t.string :email, null: false
+      t.string :encrypted_password, null: false
       t.timestamps
     end
   end
