12fe340cdc89f1cc9df3de2c32607964186ff032
tsubamenoki <144215790+tsubamenoki@users.noreply.github.com>
2023-11-18 06:47:15 +0000

    登録とログアウトのみ作成

--- app/controllers/public/sessions_controller.rb
+++ app/controllers/public/sessions_controller.rb
@@ -18,6 +18,13 @@ class Public::SessionsController < Devise::SessionsController
   #   super
   # end
 
+  def after_sign_in_path_for(resource)
+    customers_mypage_path
+  end
+
+  def after_sign_out_path_for(resource)
+    new_customer_registration_path
+  end
   # protected
 
   # If you have extra params to permit, append them to the sanitizer.
