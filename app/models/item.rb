d57ddac5f7712c5710cfeb26a51c607dc51d1709
tsubamenoki <144215790+tsubamenoki@users.noreply.github.com>
2023-11-19 02:29:44 +0000

    リレーション反映

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,10 @@
 class Item < ApplicationRecord
+
   has_one_attached :image
+
+  has_many :car_items
+  has_many :order_details
+  has_many :orders, through: :order_details #n:mの関係を表現するアソシエーション。
+  belongs_to :genre
+
 end
