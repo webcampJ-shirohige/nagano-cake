6822d74f331a250279f5cbeeb66a8b84585d85c3
zzriku <zzriku@users.noreply.github.com>
2023-11-19 04:33:30 +0000

    addressのviewファイルの追加

--- app/controllers/public/addresses_controller.rb
+++ app/controllers/public/addresses_controller.rb
@@ -1,2 +1,42 @@
 class Public::AddressesController < ApplicationController
-end
+  before_action :authenticate_customer!
+  def index
+    @address = Address.new
+    @customer = current_customer
+    @addresses = @customer.addresses(params:[:id])
+  end
+
+  def create
+    @address = Address.new(address_params)
+    @address.customer_id = current_customer.id
+    if @address.save
+      redirect_to addresses_path
+    else
+      @customer = current_customer
+      @addresses = @customer.addresses(params[:id])
+      render :index
+    end
+
+    def edit
+      @address = Address.find(params[:id])
+    end
+
+    def update
+      @address = Address.find(params[:id])
+      @address.update(address_params)
+      redirect_to addresses_path
+    end
+
+    def destroy
+      address = Address.find(params[:id])
+      address.destroy
+      redirect_to addresses_path
+    end
+  end
+
+    private
+
+    def address_params
+      params.require(:address).permit(:shipping_post_code, :shipping_address, :shpping_name)
+    end
+  end
--- app/models/customer.rb
+++ app/models/customer.rb
@@ -6,6 +6,6 @@ class Customer < ApplicationRecord
 
   has_many :orders, dependent: :destroy
   has_many :cart_items, dependent: :destroy
-  has_many :addresses, dependent: :destoroy
+  has_many :addresses, dependent: :destroy
 
 end
