e100d7795d729b6763afb34ad68c66415a533a8a
tsubamenoki <144215790+tsubamenoki@users.noreply.github.com>
2023-11-20 06:01:43 +0000

    items create

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -1,2 +1,31 @@
 class Admin::ItemsController < ApplicationController
+
+  def index
+    @items=Item.all
+  end
+
+  def new
+    @item=Item.new
+  end
+
+  def create
+    item=Item.new(item_params)
+    item.save
+    redirect_to admin_items_path(item.id)
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:image, :name, :introduction, :price_before_tax)
+  end
 end
