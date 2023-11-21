bd31dc15fd0463d0079bdd11c6db287db6bf5793
tomoya2051 <f.tomoya_2051@icloud.com>
2023-11-21 06:15:22 +0000

    ヘッダー、アバウト、トップの作成（仮）

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -1,2 +1,23 @@
 class Admin::CustomersController < ApplicationController
+  def index
+    @customers = Customer.all
+  end
+
+  def show
+    @cudtomer= Customer.find(params[:id])
+  end
+
+  def edit
+    @customer = Customer.find(params[:id])
+  end
+
+
+  def update
+    @customer = Customer.find(params[:id])
+   if @customer.update(customer_params)
+        redirect_to admin_customer_path(@customer.id)
+   else
+      render :edit
+   end
+  end
 end
