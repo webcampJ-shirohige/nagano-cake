d00c73f43d497ebbf3fe84222a75710160f93f58
zzriku <zzriku@users.noreply.github.com>
2023-11-21 13:59:45 +0000

    view作成

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -1,2 +1,25 @@
 class Public::CartItemsController < ApplicationController
+  before_action :authenticate_customer!
+
+  def index
+    @cart_items = current_customer.cart_items
+    @total_payment = 0
+  end
+
+  def update
+  end
+
+  def create
+  end
+
+  def destroy
+  end
+
+  def destroy_all
+  end
+
+  private
+
+  def cart_item_params
+  end
 end
