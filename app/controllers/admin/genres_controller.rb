a2f9b6918d46375e468a39e2ed5fc626487f87c7
MasahiroKimura <143923070+masa558@users.noreply.github.com>
2023-11-22 11:51:08 +0000

    save4

--- app/controllers/admin/genres_controller.rb
+++ app/controllers/admin/genres_controller.rb
@@ -1,25 +1,32 @@
 class Admin::GenresController < ApplicationController
   def index
     @genre = Genre.new
-    @genres = Genre.all  
+    @genres = Genre.all
   end
-  
+
   def create
     @genre = Genre.new(genre_params)
-    @genre.save
-    redirect_to admin_genres_path
+    @genres = Genre.all
+    if @genre.save
+      redirect_to admin_genres_path
+    else
+      render :index
+    end
   end
-  
+
   def edit
     @genre = Genre.find(params[:id])
   end
-  
+
   def update
-    genre = Genre.find(params[:id])
-    genre.update(genre_params)
-    redirect_to admin_genres_path
+    @genre = Genre.find(params[:id])
+    if @genre.update(genre_params)
+      redirect_to admin_genres_path
+    else
+      render :edit
+    end
   end
-  
+
   private
   # ストロングパラメータ
   def genre_params
