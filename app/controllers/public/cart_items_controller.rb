dcae0fa94f8754ad8673a79fa36ad565aad5b9ae
tomoya2051 <f.tomoya_2051@icloud.com>
2023-11-23 03:29:46 +0000

    カート内商品削除機能追加

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -25,9 +25,14 @@ class Public::CartItemsController < ApplicationController
   end
 
   def destroy
+    @cart_item = CartItem.find(params[:id])
+    @cart_item.destroy
+    redirect_to cart_items_path
   end
 
   def destroy_all
+    current_customer.cart_items.destroy_all
+    redirect_to cart_items_path
   end
 
   private
