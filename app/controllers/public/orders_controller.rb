f04a1a92a1e958483f1ccfea882e704af9e4c0b5
tsubamenoki <144215790+tsubamenoki@users.noreply.github.com>
2023-11-24 03:25:30 +0000

    muri

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -5,26 +5,27 @@ class Public::OrdersController < ApplicationController
   end
 
   def confirm
-    if address_option=0
+    if params[:order][:address_option]=="0"
       @order = Order.new(order_params)
       @order.post_code = current_customer.post_cord
       @order.address = current_customer.address
       @order.name = current_customer.first_name + current_customer.last_name
-    elsif address_option=1
+    elsif params[:order][:address_option]=="1"
       @order = Order.new(order_params)
       @address = Address.find(params[:order][:address_id])
-      @order.post_code = @address.post_code
-      @order.address = @address.address
-      @order.name = @address.name
+      @order.post_code = @address.shipping_post_code
+      @order.address = @address.shipping_address
+      @order.name = @address.shipping_name
     else
       @order = Order.new(order_params)
     end
-    @cart_items=current_customer.cart_items
+    @cart_items=current_customer.cart_items.all
+    @postage=@order.postage
   end
 
   private
 
   def order_params
-    params.require(:order).permit(:payment, :post_code, :address, :name)
+    params.require(:order).permit(:payment, :post_code, :address, :name, :postage)
   end
 end
