7eb993434059bdd0ba9b4ecb85d7d03a939f1006
tomoya2051 <f.tomoya_2051@icloud.com>
2023-11-28 03:14:46 +0000

    新着商品の数制限追加

--- app/controllers/public/homes_controller.rb
+++ app/controllers/public/homes_controller.rb
@@ -1,6 +1,6 @@
 class Public::HomesController < ApplicationController
   def top
-    @items=Item.all.order(created_at: :desc)
+    @items=Item.all.order(created_at: :desc).limit(4)
   end
 
   def about
